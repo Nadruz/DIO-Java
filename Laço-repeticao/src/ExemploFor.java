3ea9dd862084a1f2a291707f4384c2416d21a406
Nadruz <juliananadruz@gmail.com>
2025-03-17 20:09:26 -0300

    Estruturas de Repetição

---
+++ Laço-repeticao/src/ExemploFor.java
@@ -0,0 +1,9 @@
+public class ExemploFor {
+    
+    public static void main(String[] args) {
+        
+        for (int contarNumeros = 1; contarNumeros <=10; contarNumeros ++) {
+            System.out.println("Conatando número " + contarNumeros);
+        }
+    }
+}
--- Metodos/src/Usuario.java
+++ Metodos/src/Usuario.java
@@ -1,8 +1,6 @@
-package Metodos;
-
-public class Usuarios {
+public class Usuario {
     
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         
         SmartTv smartTv = new SmartTv();
 
@@ -14,20 +12,19 @@ public class Usuarios {
         System.out.println("Canal atual: " + smartTv.canal);
 
         smartTv.mudarCanal(13);
-
+        
         System.out.println("Canal atual: " + smartTv.canal);
 
-        System.out.println("Volume Atual: " + smartTv.volume);
+        System.out.println("Volume atual: " + smartTv.volume);
 
         System.out.println("TV ligada? " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
         System.out.println("Volume atual: " + smartTv.volume);
-        
 
         smartTv.ligar();
-        System.out.println("Novo status -> Tv ligada? " + smartTv.ligada);
+        System.out.println("Novo status -> TV ligada? " + smartTv.ligada);
 
         smartTv.desligar();
-        System.out.println("Novo status -> Tv ligada? " + smartTv.ligada);
+        System.out.println("Novo status -> TV ligada? " + smartTv.ligada);
     }
-
 }
